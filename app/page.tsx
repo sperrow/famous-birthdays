cec6e066621dd439b85e369cae93646031845978
Matthew Sperry <mattjsperry@gmail.com>
2024-09-13 15:13:20 -0700

    add About link on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 'use client';
 
-import { Container } from '@radix-ui/themes';
+import { Button, Container, Flex } from '@radix-ui/themes';
 import DatePicker from '@/app/ui/datePicker';
 import NavMenu from '@/app/ui/navMenu';
 import Header from '@/app/ui/header';
+import Link from 'next/link';
 
 export default function Home() {
     return (
@@ -12,6 +13,13 @@ export default function Home() {
                 <NavMenu />
                 <Header />
                 <DatePicker></DatePicker>
+                <Flex direction="column" align="center" mt="6">
+                    <Link href="/about" className="pb-4">
+                        <Button size="1" variant="soft">
+                            About
+                        </Button>
+                    </Link>
+                </Flex>
             </Container>
         </main>
     );
